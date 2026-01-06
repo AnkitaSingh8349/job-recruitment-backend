35fe1be48eee59ae937694ecb4fcff3643f544c5
AnkitaSingh8349 <ankita@ajxtechnologies.com>
2026-01-06 11:09:30 +0530

    Fix urls.py and enable swagger for all apps

--- job_recruitment/urls.py
+++ job_recruitment/urls.py
@@ -1,21 +1,6 @@
-"""
-URL configuration for job_recruitment project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -27,16 +12,19 @@ schema_view = get_schema_view(
         description="All APIs in one place",
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
+    # Admin
     path("admin/", admin.site.urls),
 
-    # 🔹 ALL app APIs
-    path("api/", include("accounts.urls")),
+    # APIs
+    path("api/accounts/", include("accounts.urls")),
+    path("api/jobs/", include("jobs.urls")),
+    path("api/applications/", include("applications.urls")),
 
-    # 🔹 DOCS
-    path("docs/", schema_view.with_ui("swagger", cache_timeout=0)),
+    # API Docs
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0)),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
 ]
